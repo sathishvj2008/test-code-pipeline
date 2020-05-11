9230aff85c2ed769d8b71c5baf65f3127130d504
Sathish VJ <sathish.vj@cognizant.com>
2020-05-11 14:54:08 +0530

    first commit

---
+++ src/main/java/com/example/aws/AwsElasticBeanStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanStackApplication.class, args);
+	}
+
+}
